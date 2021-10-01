90c98307cc9e918ebb7bc2494129bb87dd7e4af7
PRINCETITAN\JRJason <jrreinerie@gmail.com>
2021-10-01 21:49:28 +0200

    Project to github

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function mainMethod(): Response
+    {
+        return $this->render('homePage.html.twig', [
+            'fName' => 'Jason',
+            'lName' => 'Reinerie',
+        ]);
+    }
+}
